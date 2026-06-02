e3d4e386d30212522551122ea0e7f7fb98de67e6
Thai Hua <huavietthai299@gmail.com>
2026-06-02 22:10:24 +0700

    fix(cors): enable cross-origin resource sharing for web client

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const config = new DocumentBuilder()
